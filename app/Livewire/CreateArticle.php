c9e278470927439d31c03809bcc9e3ce4c339101
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-23 16:57:19 +0200

    aggiunta logica gestione create livewire

--- app/Livewire/CreateArticle.php
+++ app/Livewire/CreateArticle.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class CreateArticle extends Component
 {
+      public $title;
+    public $price;
+    public $description;
+
+    protected $rules = [
+        'title' => 'required|string|max:255',
+        'price' => 'required|numeric|min:0',
+        'description' => 'required|string',
+    ];
+
+    public function create()
+    {
+        $validated = $this->validate();
+
+        Article::create([
+            'title' => $validated['title'],
+            'price' => $validated['price'],
+            'description' => $validated['description'],
+            'user_id' => Auth::id(),
+        ]);
+
+        session()->flash('success', 'Articolo creato con successo.');
+        return redirect()->route('articles.index');
+    }
     public function render()
     {
         return view('livewire.create-article');
