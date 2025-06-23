dac767c5397e2999fd261c6218de11a44492dfed
Jamila bouazzaoui <jiji.panda90@gmail.com>
2025-06-23 16:29:58 +0200

    aggiunte migrazioni e viste article

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view ('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArticleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        return view('articles.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
+        abort_if($article->user_id !== auth()->id(), 403, 'Non sei autorizzato ad accedere a questa pagina.');
+        return view('articles.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
+    {
+        abort_if($article->user_id !== auth()->id(), 403, 'Non sei autorizzato ad accedere a questa pagina.');
+
+        $article->update($request->validated());
+
+        return redirect()->route('articles.show', $article)->with('success', 'Articolo aggiornato con successo.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        abort_if($article->user_id !== auth()->id(), 403, 'Non sei autorizzato ad accedere a questa pagina.');
+
+        $article->delete();
+
+        return redirect()->route('articles.index')->with('success', 'Articolo eliminato con successo.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PublicController::class, 'home'])->name('home');
+Route::get('/article/create',[ArticleController::class, 'create'])->name('article.create');
+Route::get('article/{article}', [ArticleController::class, 'show'])->name('article.show');
+Route::get('article/index', [ArticleController::class, 'index'])->name('article.index');
+Route::get('article/{article}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+Route::delete('article/{article}', [ArticleController::class, 'destroy'])->name('article.destroy');
