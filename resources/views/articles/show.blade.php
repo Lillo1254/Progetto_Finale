4817c472054242bd601d5d964a96db15824b33dc
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-24 12:48:36 +0200

    iniziato front-end carousel

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,28 +1,30 @@
 <x-layout>
-    <div class="container mt-5">
-        <div class="row d-flex align-items-center">
-            <div class="col-12 col-md-6">
+    <div class="container-fluid mt-5">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-12 col-md-6 col-lg-5 d-flex flex-column align-items-center justify-content-center">
                 <h1 class="text-dark">Dettagli articolo: {{ $article->title }}</h1>
                 <h2 class="text-dark">Inserito da:</h2>
                 <h5 class="text-dark">{{ $article->user->name }}</h5>
                 <p class="text-dark">{{ $article->description }}</p>
                 <p class="text-dark fw-bold">💰 Prezzo: € {{ number_format($article->price, 2) }}</p>
             </div>
+        </div>
+    </div>
 
     {{-- Carosello immagini --}}
     <div class="container-fluid">
       <div class="row w-100 justify-content-center align-items-center">
-        <div class="col-12 col-md-6 col-lg-5 d-flex justify-content-center align-items-center">
+        <div class="col-12 col-md-6 col-lg-6 d-flex justify-content-center align-items-center ">
           <div id="articleCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://picsum.photos/300/301" class="d-block img-fluid rounded" alt="Immagine articolo">
+                <img src="https://picsum.photos/500/401" class="d-block img-fluid rounded" alt="Immagine articolo">
             </div>
             <div class="carousel-item active">
-                <img src="https://picsum.photos/300/302" class="d-block img-fluid rounded" alt="Immagine articolo">
+                <img src="https://picsum.photos/500/402" class="d-block img-fluid rounded" alt="Immagine articolo">
             </div>
             <div class="carousel-item active">
-                <img src="https://picsum.photos/300/303" class="d-block img-fluid rounded" alt="Immagine articolo">
+                <img src="https://picsum.photos/500/403" class="d-block img-fluid rounded" alt="Immagine articolo">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#articleCarousel" data-bs-slide="prev">
