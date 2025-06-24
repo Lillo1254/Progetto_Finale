f0c226db5bc2451ef3af828ac78155159400c3fe
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-24 11:40:35 +0200

    aggiunta visualizzazione categorie

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,7 +13,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::orderby('created_at', 'desc')->get();
         return view('articles.catalogo', compact('articles'));
     }
 
