bf9355d6f7cd023b6c69076857cd89adf22815c4
SIMEONE FIGLIOLIA <simeonefigliolia@gmail.com>
2025-06-25 16:21:34 +0200

    sistemazione responsive footer home e form al crea articolo

--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -1,79 +1,75 @@
-
-    <div class="primary-bg min-vh-100">
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-12">
-                    <h1 class="secondary-text display-3 pt-5 text-center">Crea un Articolo</h1>
-                </div>
+<div class="primary-bg min-vh-100 d-flex flex-column justify-content-center pb-5">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-12">
+                <h1 class="secondary-text display-3 pt-5 text-center">Crea un Articolo</h1>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-10 col-lg-8">
-                    <form class="primary-light-bg p-5 rounded-4" wire:submit.prevent="create">
-                        @csrf
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10 col-lg-8">
+                <form class="primary-light-bg p-5 rounded-4 mb-5" wire:submit.prevent="create">
+                    @csrf
 
-                        @if (session()->has('success'))
-                            <div class="alert alert-success m-0 mb-4">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-
-                        @if ($errors->any())
-                            <div class="alert alert-danger m-0 mb-4">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <!-- Titolo -->
-                        <div class="mb-3">
-                            <label for="title" class="form-label">Titolo</label>
-                            <input type="text" class="form-control" id="title" wire:model="title" required>
-                            @error('title')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                    @if (session()->has('success'))
+                        <div class="alert alert-success m-0 mb-4">
+                            {{ session('success') }}
                         </div>
+                    @endif
 
-                        <!-- Prezzo -->
-                        <div class="mb-3">
-                            <label for="price" class="form-label">Prezzo</label>
-                            <input type="number" class="form-control" id="price" wire:model="price" step="0.01"
-                                required>
-                            @error('price')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                    @if ($errors->any())
+                        <div class="alert alert-danger m-0 mb-4">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
+                    @endif
 
-                        <!-- Descrizione -->
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Descrizione</label>
-                            <textarea class="form-control text-dark" id="description" wire:model="description" required></textarea>
-                            @error('description')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                    <!-- Titolo -->
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Titolo</label>
+                        <input type="text" class="form-control" id="title" wire:model="title" required>
+                        @error('title')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <!-- Prezzo -->
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Prezzo</label>
+                        <input type="number" class="form-control" id="price" wire:model="price" step="0.01" required>
+                        @error('price')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <!-- Descrizione -->
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Descrizione</label>
+                        <textarea class="form-control text-dark" id="description" wire:model="description" required></textarea>
+                        @error('description')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    @foreach ($categories as $category)
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="category_id"
+                                id="category-{{ $category->id }}" value="{{ $category->id }}"
+                                wire:model="category_id">
+                            <label class="form-check-label" for="category-{{ $category->id }}">
+                                {{ $category->name }}
+                            </label>
                         </div>
+                    @endforeach
 
-                        @foreach ($categories as $category)
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="category_id"
-                                    id="category-{{ $category->id }}" value="{{ $category->id }}"
-                                    wire:model="category_id">
-                                <label class="form-check-label" for="category-{{ $category->id }}">
-                                    {{ $category->name }}
-                                </label>
-                            </div>
-                        @endforeach
-
-                        <!-- Pulsante di invio -->
-                        <div
-                            class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
-                            <button type="submit" class="btn btn-form mt-5 px-5">Crea Articolo</button>
-                        </div>
-                    </form>
-                </div>
+                    <!-- Pulsante di invio -->
+                    <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
+                        <button type="submit" class="btn btn-form mt-5 px-5">Crea Articolo</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
+</div>
