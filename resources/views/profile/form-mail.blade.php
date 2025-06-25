b228992321d00194f3cec7faa1014834b7752e11
Luca Simone <lucasimonecomposer@gmail.com>
2025-06-25 17:41:10 +0200

    started frontend on revisor request form

--- resources/views/profile/form-mail.blade.php
+++ resources/views/profile/form-mail.blade.php
@@ -1,12 +1,19 @@
 <x-layout> 
- 
-  <form action="{{ route('revisor.request') }}" method="POST">
-    @csrf
-    <input type="hidden" name="is_revisor" value="1">
-    <button type="submit" class="btn btn-primary">
-        Richiedi di diventare revisore
-    </button>
-</form>
 
+<div class="primary-bg">
+  <div class="min-vh-100 container">
+    <div class="row justify-content-center py-5">
+      <div class="col-12 col-md-10 col-lg-8 text-center">
+        <form action="{{ route('revisor.request') }}" method="POST">
+          @csrf
+          <input type="hidden" name="is_revisor" value="1">
+          <button type="submit" class="btn btn-primary">
+            Richiedi di diventare revisore
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
  
  </x-layout>
