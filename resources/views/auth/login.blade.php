4d96757fecdc022a743bead7f2b2f0938cf44399
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-25 09:59:59 +0200

    aggiunto bordo register e login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
-<x-layout> 
- 
+<x-layout>
+
     <div class="primary-bg min-vh-100">
         <div class="container">
             <div class="row mb-5">
@@ -9,7 +9,7 @@
             </div>
             <div class="row justify-content-center">
                 <div class="col-12 col-md-10 col-lg-8">
-                    <form class="primary-light-bg p-5 rounded-4" action="{{route('login')}}" method="POST">
+                    <form class="primary-light-bg p-5 rounded-4 form-glow" action="{{ route('login') }}" method="POST">
                         @csrf
                         @if ($errors->any())
                             <div class="alert alert-danger m-0 mb-4">
@@ -23,16 +23,22 @@
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" name="email">
-                            <div id="emailHelp" class="form-text secondary-text">We'll never share your email with anyone else.</div>
-                            </div>
+                            <div id="emailHelp" class="form-text secondary-text">We'll never share your email with
+                                anyone else.</div>
+                        </div>
                         <div>
                             <label for="exampleInputPassword1" class="form-label">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                         </div>
-                        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
+                        <div
+                            class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
                             <button type="submit" class="btn btn-form mt-5 px-5">Submit</button>
-                            <p class="my-0 mt-5 secondary-text">Don't have an account? <a class="form-link secondary-text text-decoration-none" href="{{route('register')}}">Sign up</a></p>
+                            <p class="my-0 mt-5 secondary-text">Don't have an account? <a
+                                    class="form-link secondary-text text-decoration-none"
+                                    href="{{ route('register') }}">Sign up</a></p>
                         </div>
+                        <div class="left-glow"></div>
+                        <div class="bottom-glow"></div>
                     </form>
                 </div>
             </div>
@@ -40,5 +46,5 @@
 
     </div>
 
- 
- </x-layout>
+
+</x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
-<x-layout> 
- 
+<x-layout>
+
     <div class="primary-bg min-vh-100">
         <div class="container">
             <div class="row mb-5">
@@ -8,8 +8,8 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-12 col-md-10 col-lg-8">
-                    <form class="primary-light-bg p-5 rounded-4" action="{{route('register')}}" method="POST">
+                <div class="col-12 col-md-10 col-lg-8 ">
+                    <form class="primary-light-bg p-5 rounded-4 form-glow" action="{{ route('register') }}" method="POST">
                         @csrf
                         @if ($errors->any())
                             <div class="alert alert-danger m-0 mb-4">
@@ -26,7 +26,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Indirizzo Email</label>
-                            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" required>
+                            <input type="email" class="form-control" id="email" name="email"
+                                aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
@@ -34,12 +35,18 @@
                         </div>
                         <div class="mb-3">
                             <label for="password_confirmation" class="form-label">Conferma Password</label>
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                            <input type="password" class="form-control" id="password_confirmation"
+                                name="password_confirmation" required>
                         </div>
-                        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
+                        <div
+                            class="d-flex flex-column flex-lg-row align-items-center justify-content-between m-0 my-auto p-0">
                             <button type="submit" class="btn btn-form mt-5 px-5">Register</button>
-                            <p class="my-0 mt-5 secondary-text">Already have an account? <a class="form-link secondary-text text-decoration-none" href="{{route('login')}}">Login</a></p>
+                            <p class="my-0 mt-5 secondary-text">Already have an account? <a
+                                    class="form-link secondary-text text-decoration-none"
+                                    href="{{ route('login') }}">Login</a></p>
                         </div>
+                        <div class="left-glow"></div>
+                        <div class="bottom-glow"></div>
                     </form>
                 </div>
             </div>
