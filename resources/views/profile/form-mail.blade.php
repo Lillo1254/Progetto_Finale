6cfe0a9f1778df5152ff1ec7d67d619257a91345
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-25 17:02:14 +0200

    sistemazione email

--- resources/views/profile/form-mail.blade.php
+++ resources/views/profile/form-mail.blade.php
@@ -2,6 +2,7 @@
  
   <form action="{{ route('revisor.request') }}" method="POST">
     @csrf
+    <input type="hidden" name="is_revisor" value="1">
     <button type="submit" class="btn btn-primary">
         Richiedi di diventare revisore
     </button>
