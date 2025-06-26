5730845fc0fa7a5db2616761e04e28fd2974a9a3
alessandro lucia <alessandrolucia75@gmail.com>
2025-06-26 10:38:36 +0200

    sistemata pagina revisore

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -9,8 +9,8 @@ class RevisorController extends Controller
 {
     public function revisorProfile()
     {
-        $articles_to_check = Article::where('is_accepted', null)->orderby('created_at', 'asc')->get();
-        return view('revisor.index', compact('articles_to_check'));
+        $article_to_check = Article::where('is_accepted', null)->orderby('created_at', 'asc')->get();
+        return view('revisor.index', compact('article_to_check'));
     }
     public function accept(Article $article)
     {
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -12,13 +12,13 @@
 
         <div class="rcontainer-fluid justify-content-center pt-5">
             <div class="row justify-content-center">
-                @for ($i = 0; $i < 4; $i++)
+                @forelse($article_to_check->take(4) as $article_to_check)
                     <div
                         class="col-6 col-md-4 mb-4 text-center d-flex flex-column align-items-center justify-content-between">
 
 
                         <img src="https://picsum.photos/300" class="img-fluid rounded-4 mb-3" alt="immagine default">
-                        <h1 text-center>{{ $article_to_check->title }}</h1>
+                        <h1 text-center>{{ $article_to_check->title}}</h1>
                         <h4 class="text-muted text-center">{{ $article_to_check->category->name }}</h4>
                         <p class="h6 text-center">{{ $article_to_check->description }}</p>
                         <div class="d-flex pb-4 justify-content-around">
@@ -35,18 +35,17 @@
                     </div>
             </div>
         </div>
-    @endfor
 
-    @endif
-@else
+@empty
     <div class="row justify-content-center align-items-center height-custom text-center">
         <div class="col-12">
             <h1 class="fst-italic display-4">
                 Nessun articolo da revisionare
             </h1>
-            <a href="{{ route('homepage') }}" class="mt-5 btn btn-success">Torna all'homepage</a>
+            <a href="{{ route('home') }}" class="mt-5 btn btn-success">Torna all'homepage</a>
         </div>
     </div>
+    @endforelse
     @endif
 
 
