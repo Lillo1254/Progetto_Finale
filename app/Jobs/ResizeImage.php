225d94743ef6b03e925c88117dbc2eb0973f26ab
alessandro lucia <alessandrolucia75@gmail.com>
2025-07-02 16:55:50 +0200

    implementato watermark

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,12 +19,10 @@ class ResizeImage implements ShouldQueue
     private $w, $h, $fileName, $path;
     public function __construct($filePath, $w, $h)
     {
-        $this->path =dirname($filePath);
-        $this->fileName =basename($filePath);
-        $this->w =$w;
-        $this->h =$h;
-
-
+        $this->path = dirname($filePath);
+        $this->fileName = basename($filePath);
+        $this->w = $w;
+        $this->h = $h;
     }
 
     /**
@@ -31,14 +30,21 @@ class ResizeImage implements ShouldQueue
      */
     public function handle(): void
     {
-        $w=$this->w;
-        $h=$this->h;
-        $srcPath=storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
-        $destPath=storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
+        $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
         Image::load($srcPath)
-        ->crop($w,$h,CropPosition::Center)
-        ->save($destPath);
-        
+            ->crop($w, $h, CropPosition::Center)
+            ->watermark(
+                base_path('resources/img/Logo_LightMode.png'),
+                width: 50,
+                height: 50,
+                paddingX: 5,
+                paddingY: 5,
+                paddingUnit: Unit::Percent
+            )
+            ->save($destPath);
     }
 }
